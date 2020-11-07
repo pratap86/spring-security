e9d16ed9b972e97c14553e5468fb060ac0e07ec4
pratap86 <narayanpratap86@gmail.com>
2020-11-07 13:45:06 +0530

    refactor as per publicKey

--- authserver/src/main/java/com/pratap/springcloud/security/config/AuthorizationServerConfig.java
+++ authserver/src/main/java/com/pratap/springcloud/security/config/AuthorizationServerConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -53,6 +54,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient("couponclientapp").secret(passwordEncoder.encode("9999"))
 				.authorizedGrantTypes("password", "refresh_token").scopes("read", "write").resourceIds(RESOURCE_ID);
+	}
+	
+	
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()");
 	}
 
 	@Bean
--- couponservice/src/main/java/com/pratap/springcloud/security/oath2/config/ResourceServerConfig.java
+++ couponservice/src/main/java/com/pratap/springcloud/security/oath2/config/ResourceServerConfig.java
@@ -1,16 +1,11 @@
 package com.pratap.springcloud.security.oath2.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
@@ -20,8 +15,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	private static final String GET_PATTERNS = "/couponapi/coupons/{code:^[A-Z]*$}";
 	private static final String POST_PATTERNS = "/couponapi/coupons";
 	
-	@Value("${publicKey}")
-	private String publicKey;
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -34,19 +27,5 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		.mvcMatchers(HttpMethod.GET, GET_PATTERNS).hasAnyRole("USER", "ADMIN")
 		.mvcMatchers(HttpMethod.POST, POST_PATTERNS).hasRole("ADMIN")
 		.anyRequest().denyAll().and().csrf().disable();
-	}
-	
-	@Bean
-	public JwtAccessTokenConverter jwtAccessTokenConverter() {
-		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-
-		jwtAccessTokenConverter.setVerifierKey(publicKey);
-		
-		return jwtAccessTokenConverter;
-	}
-
-	@Bean
-	public TokenStore tokenStore() {
-		return new JwtTokenStore(jwtAccessTokenConverter());
 	}
 }
